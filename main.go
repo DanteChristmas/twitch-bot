43951be64fb487066b82459fd174edc4738d51c8
dantechristmas <jjs5286@gmail.com>
2021-07-10 23:12:02 -0600

    trying to get twitch irc to acknowledge me

--- main.go
+++ main.go
@@ -1,15 +1,18 @@
 package main
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
 	"io"
+	"net"
 	"net/http"
 	"os"
 	"strings"
 )
 
-func main() {
+func main() { 
+	// Getting a token
 	var keys struct{
 		Id string `json:"client_id"`
 		Secret string `json:"client_secret"`
@@ -25,10 +28,11 @@ func main() {
 	dec.Decode(&keys)
 	fmt.Println(keys)
 
-	req, err := http.NewRequest("POST", fmt.Sprintf("https://id.twitch.tv/oauth2/token?client_id=%s&client_secret=%s&grant_type=client_credentials", keys.Id, keys.Secret), strings.NewReader(""))
+	req, err := http.NewRequest("POST", fmt.Sprintf("https://id.twitch.tv/oauth2/token?client_id=%s&client_secret=%s&grant_type=client_credentials&scopes=chat:read,chat:edit", keys.Id, keys.Secret), strings.NewReader(""))
 	if err != nil {
 		panic(err)
 	}
+	req.Header.Add("content-type", "application/json")
 
 	var client http.Client
 
@@ -38,5 +42,27 @@ func main() {
 	}
 	defer res.Body.Close()
 
-	io.Copy(os.Stdout, res.Body)
+	var token struct {
+		Token string `json:"access_token"`
+		Expires int `json:"expires_in"`
+		TokenType string `json:"token_type"`
+	}
+
+	json.NewDecoder(res.Body).Decode(&token)
+
+	// Connecting to IRC
+	c,err := net.Dial("tcp", "irc.chat.twitch.tv:6697")
+	if err != nil {
+		panic(err)
+	}
+	defer c.Close()
+
+	pass_message := fmt.Sprintf("PASS oauth:%s\r\n", token.Token)
+	c.Write([]byte(pass_message))
+	c.Write([]byte("NICK basic botch\r\n"))
+	c.Write([]byte("JOIN dantechristmas"))
+
+	var buf bytes.Buffer
+	io.Copy(&buf, c)
+	fmt.Println("total size:", buf.Len())
 }
